3225d23c91aa8694334c0edbdbf3e97d689b698e
Sceue <xixu@andrew.cmu.edu>
2024-01-05 07:28:44 +0000

    inference speech extraction & w/o prompt change

--- train/stage1.py
+++ train/stage1.py
@@ -124,7 +124,7 @@ class DataCollatorForSupervisedDataset(object):
     length_after_ssl: None
     length_after_adp: None
     prompt_list_asr = ['<speech_here> Try to decipher the spoken language and write it down.']
-    prompt_list_st = ['<speech_here> Start by converting the English audio into Spanish written form.','<speech_here> Start by converting the English audio into German written form.']
+    prompt_list_st = ['<speech_here>']
 
     def __call__(self, samples: List[SpeechToTextDatasetItem]) -> Dict[str, torch.Tensor]:
         # todo: sort samples by descending number of frames
@@ -138,24 +138,16 @@ class DataCollatorForSupervisedDataset(object):
         to_adds = [int(speech_len)*DEFAULT_SPEECH_PATCH_TOKEN for speech_len in speech_lens]
         to_adds = [DEFAULT_SPEECH_START_TOKEN + to_add + DEFAULT_SPEECH_END_TOKEN for to_add in to_adds]
         tasks = [x.task for x in samples]
-        prompts = []
-        for task in tasks:
-             if task == "asr":
-                 prompt = random.choice(self.prompt_list_asr)
-             elif task == "st":
-                 prompt = self.prompt_list_st[0]
-             else: # default is st
-                 prompt = self.prompt_list_st[0]
-             prompts.append(prompt)
+        # prompts = []
 
         conv = conversation_lib.default_conversation.copy()
         # conv = random.choice(list(conversation_lib.conv_templates.values())).copy()
         conversations = []
-        for prompt, to_add, text in zip(prompts, to_adds, texts):
+        for to_add, text in zip(to_adds, texts):
             conv.messages = []
-            before, after = prompt.split('<speech_here>')
-            mm_prompt = before + to_add + after
-            conv.append_message(conv.roles[0], mm_prompt)
+            # before, after = prompt.split('<speech_here>')
+            # mm_prompt = before + to_add + after
+            conv.append_message(conv.roles[0], to_add)
             conv.append_message(conv.roles[1], text)
             conversations.append(conv.get_prompt())
         input_ids = self.tokenizer(
