dd65a85105a3e632f8f7aadc0077d963848981a9
Sceue <xixu@andrew.cmu.edu>
2024-01-15 06:11:35 +0000

    change uni forward

--- train/uni_wav2vec_monkey_patch.py
+++ train/uni_wav2vec_monkey_patch.py
@@ -43,6 +43,32 @@ def generate_2d_causal_mask(seq_len, dtype, device='gpu'):
     return mask
 
 
+# def causal_forward(
+#     self,
+#     x: torch.Tensor,
+#     self_attn_mask: torch.Tensor = None,
+#     self_attn_padding_mask: torch.Tensor = None,
+#     need_weights: bool = False,
+#     att_args=None,
+# ):
+#     # Generate the causal mask
+#     # print(x)
+#     # print(x.size(2))
+#     # print(self_attn_mask)
+#     causal_mask = generate_2d_causal_mask(x.size(0), dtype=x.dtype,device=x.device)
+    
+#     if self_attn_mask is not None:
+#         self_attn_mask = self_attn_mask + causal_mask
+#     else:
+#         self_attn_mask = causal_mask
+
+#     return original_forward(
+#         self, x, 
+#         self_attn_mask=self_attn_mask, 
+#         self_attn_padding_mask=self_attn_padding_mask, 
+#         need_weights=need_weights,
+#         att_args=att_args)
+
 def causal_forward(
     self,
     x: torch.Tensor,
@@ -51,23 +77,68 @@ def causal_forward(
     need_weights: bool = False,
     att_args=None,
 ):
-    # Generate the causal mask
-    # print(x)
-    # print(x.size(2))
-    # print(self_attn_mask)
+    """
+    LayerNorm is applied either before or after the self-attention/ffn
+    modules similar to the original Transformer imlementation.
+    """
+
     causal_mask = generate_2d_causal_mask(x.size(0), dtype=x.dtype,device=x.device)
-    
     if self_attn_mask is not None:
         self_attn_mask = self_attn_mask + causal_mask
     else:
         self_attn_mask = causal_mask
+        
+    residual = x
 
-    return original_forward(
-        self, x, 
-        self_attn_mask=self_attn_mask, 
-        self_attn_padding_mask=self_attn_padding_mask, 
-        need_weights=need_weights,
-        att_args=att_args)
+    if self.layer_norm_first:
+        x = self.self_attn_layer_norm(x)
+        x, attn = self.self_attn(
+            query=x,
+            key=x,
+            value=x,
+            key_padding_mask=self_attn_padding_mask,
+            attn_mask=self_attn_mask,
+            need_weights=False,
+        )
+        x = self.dropout1(x)
+        x = residual + x
+
+        residual = x
+        x = self.final_layer_norm(x)
+        x = self.activation_fn(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        layer_result = x
+
+        x = self.dropout3(x)
+        x = residual + x
+    else:
+        x, attn = self.self_attn(
+            query=x,
+            key=x,
+            value=x,
+            key_padding_mask=self_attn_padding_mask,
+            need_weights=False,
+        )
+
+        x = self.dropout1(x)
+        x = residual + x
+
+        x = self.self_attn_layer_norm(x)
+
+        residual = x
+        x = self.activation_fn(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        layer_result = x
+
+        x = self.dropout3(x)
+        x = residual + x
+        x = self.final_layer_norm(x)
+
+    return x, (attn, layer_result)
 
 def replace_forward():
     TransformerSentenceEncoderLayer.forward = causal_forward
