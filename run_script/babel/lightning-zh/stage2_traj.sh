9e27249b655fee0a4180116a97da4a78791f6f6f
Siqi Ouyang <owaski123@gmail.com>
2024-12-20 10:36:10 -0500

    add scripts

--- run_script/babel/lightning-zh/stage2_traj.sh
+++ run_script/babel/lightning-zh/stage2_traj.sh
@@ -7,24 +7,21 @@
 #SBATCH --mem=512GB
 #SBATCH --gres=gpu:L40S:8
 ##SBATCH --nodelist=babel-3-17
-#SBATCH --exclude=babel-13-13,babel-13-29,babel-4-9,babel-3-5,babel-3-17,babel-3-9,babel-6-29,babel-11-25,babel-7-9
-#SBATCH --partition=preempt
+#SBATCH --exclude=babel-13-13,babel-13-29,babel-4-9,babel-3-5,babel-3-17,babel-3-9,babel-6-29,babel-11-25,babel-10-13,babel-7-[1,5,9]
+#SBATCH --partition=general
 #SBATCH --time=2-00:00:00
 ##SBATCH --dependency=afterok:job_id
-#SBATCH --array=2
+##SBATCH --array=1-7
 ##SBATCH --account=siqiouya
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=siqiouya@andrew.cmu.edu
 #SBATCH -e slurm_logs/%j.err
 #SBATCH -o slurm_logs/%j.out
 
-lr=1e-4
-training_sets=("comet_0.40" "comet_0.50" "comet_0.65" "comet_0.70")
-training_set=${training_sets[$SLURM_ARRAY_TASK_ID]}
-
 source /home/siqiouya/anaconda3/bin/activate speechllama
 
 llm_path=/compute/babel-4-1/siqiouya/llama-3.1-8b-hf
+sllm_weight_path=/compute/babel-5-23/siqiouya/runs/8B-traj-s1/epoch\=3-step\=4659.ckpt/
 w2v2_path=/data/user_data/siqiouya/runs/pretrained/wav2_vec_vox_960h_pl.pt
 # w2v2_path=/data/user_data/siqiouya/runs/pretrained/hubert_large_ll60k_finetune_ls960.pt
 w2v2_type=w2v2
@@ -36,12 +33,13 @@ data_path=/compute/babel-6-17/xixu/datasets/must-c-v2.0/en-zh
 
 source_lang="English"
 target_lang="Chinese"
-name="8B-s1-${target_lang,,}-${w2v2_type}-${training_set}"
+name="8B-traj-s2"
 save_path=/compute/babel-5-23/siqiouya/runs/$name
 rm -rf ${save_path}
 mkdir -p ${save_path}
 
 export PYTHONPATH=/home/siqiouya/work/sllama
+export TOKENIZERS_PARALLELISM=false
 export WANDB_PROJECT="mustc_1.0_zh"
 export WANDB_ENTITY="streamllama"
 
@@ -51,10 +49,14 @@ export TORCH_DISTRIBUTED_DEBUG=INFO
 export NCCL_DEBUG=INFO
 SLURM_GPUS=8
 
+python /home/siqiouya/work/sllama/train/zero_to_fp32.py ${sllm_weight_path} ${sllm_weight_path}/pytorch_model.bin
+python /home/siqiouya/work/sllama/train/prune_bin.py ${sllm_weight_path}/pytorch_model.bin
+
 srun python /home/siqiouya/work/sllama/train/main_lightning.py \
     \
     --w2v2_path ${w2v2_path} \
     --w2v2_type ${w2v2_type} \
+    --w2v2_freeze True \
     --ctc_finetuned ${ctc_finetuned} \
     --length_shrink_cfg "[(1024,2,2)] * 2" \
     --block_size 48 \
@@ -62,28 +64,26 @@ srun python /home/siqiouya/work/sllama/train/main_lightning.py \
     --xpos False \
     \
     --llm_path ${llm_path} \
-    --llm_freeze True \
-    --llm_emb_freeze False \
-    --orig_embeds_params True \
+    --sllm_weight_path ${sllm_weight_path}/pytorch_model.bin \
     \
     --data_path ${data_path} \
-    --data_split_train ${training_set} \
-    --data_split_eval 'dev' \
+    --data_split_train 'comet_0.50_traj' \
+    --data_split_eval 'dev_traj' \
     --source_lang "${source_lang}" \
     --target_lang "${target_lang}" \
     \
     --seed 998244353 \
-    --stage 1 \
-    --train_bsz 800000 \
-    --eval_bsz 800000 \
-    --learning_rate ${lr} \
-    --warmup_steps 1000 \
+    --stage 2 \
+    --train_bsz 640000 \
+    --eval_bsz 640000 \
+    --learning_rate 7e-6 \
+    --warmup_steps 100 \
     --run_name $name \
     \
     --n_device ${SLURM_GPUS} \
     --deepspeed_stage 2 \
-    --max_epochs 6 \
-    --grad_acc_steps 4 \
+    --max_epochs 1 \
+    --grad_acc_steps 5 \
     --clip_norm 1.0 \
     --save_dir ${save_path} \
     --log_step 5 \
