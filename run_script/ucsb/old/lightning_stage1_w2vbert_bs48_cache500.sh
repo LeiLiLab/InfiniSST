6c40cb9b0fbef6dd17610e72aae5c3d7e7697ab2
Siqi Ouyang <owaski123@gmail.com>
2025-01-04 06:51:55 +0000

    reorgnize train scripts

---
+++ run_script/ucsb/old/lightning_stage1_w2vbert_bs48_cache500.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=256GB
+#SBATCH --gpus=8
+##SBATCH --constraint=xeon-4116 
+#SBATCH --partition=aries
+#SBATCH --time=7-00:00:00
+##SBATCH --dependency=afterok:job_id
+##SBATCH --array=1-7
+#SBATCH --account=siqiouyang
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=siqiouya@andrew.cmu.edu
+#SBATCH --output=slurm_logs/slurm-%j.out
+
+source /mnt/taurus/home/siqiouyang/anaconda3/bin/activate /mnt/taurus/home/siqiouyang/anaconda3/envs/speechllama
+
+llm_path=/mnt/taurus/data/siqiouyang/download/llama3.1-8b-hf/
+w2v2_path=/mnt/taurus/data/siqiouyang/download/w2v-bert-2.0
+w2v2_type=w2v-bert
+# data_path=/scratch/xixu/dataset/must-c-v1.0/en-es
+data_path=/mnt/aries/data/siqiouyang/datasets/must-c-v1.0
+# data_path=/compute/babel-6-17/xixu/datasets/must-c-v1.0/en-fr
+
+
+source_lang="English"
+target_lang="German"
+name="3.1-8B-s1-lightning-${target_lang,,}-${w2v2_type}"
+save_path=/mnt/taurus/data/siqiouyang/runs/sllama/en-de/$name
+rm -rf ${save_path}
+mkdir -p ${save_path}
+
+export PYTHONPATH=/home/siqiouyang/work/projects/sllama
+export WANDB_PROJECT="mustc_1.0_de"
+export WANDB_ENTITY="streamllama"
+
+export TOKENIZERS_PARALLELISM=false
+
+export NCCL_P2P_DISABLE=0
+export NCCL_IB_DISABLE=0
+SLURM_GPUS=8
+
+srun python /home/siqiouyang/work/projects/sllama/train/main_lightning.py \
+    \
+    --w2v2_path ${w2v2_path} \
+    --w2v2_type ${w2v2_type} \
+    --length_shrink_cfg "[(1024,2,2)] * 2" \
+    --block_size 48 \
+    --max_cache_size 500 \
+    \
+    --llm_path ${llm_path} \
+    --llm_freeze True \
+    \
+    --data_path ${data_path} \
+    --data_split_train 'train_st_de_mfa_llama3' \
+    --data_split_eval 'dev_st_de_mfa_llama3' \
+    --source_lang "${source_lang}" \
+    --target_lang "${target_lang}" \
+    \
+    --seed 998244353 \
+    --stage 1 \
+    --train_bsz 800000 \
+    --eval_bsz 800000 \
+    --learning_rate 2e-4 \
+    --warmup_steps 1000 \
+    --run_name $name \
+    \
+    --n_device ${SLURM_GPUS} \
+    --deepspeed_stage 2 \
+    --max_epochs 6 \
+    --grad_acc_steps 4 \
+    --clip_norm 1.0 \
+    --save_dir ${save_path} \
+    --log_step 5 \
+    --eval_step 200
---
+++ run_script/ucsb/old/run_stage1_w2v2rope_cacheinf.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=256GB
+#SBATCH --gpus=8
+##SBATCH --constraint=xeon-4116 
+#SBATCH --partition=aries
+#SBATCH --time=7-00:00:00
+##SBATCH --dependency=afterok:job_id
+##SBATCH --array=1-7
+#SBATCH --account=siqiouyang
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=siqiouya@andrew.cmu.edu
+#SBATCH --output=slurm_logs/slurm-%j.out
+
+source /home/siqiouyang/anaconda3/bin/activate /home/siqiouyang/anaconda3/envs/sllama_lightning
+
+# llm_model=meta-llama/Llama-2-7b-hf
+llm_path=/mnt/taurus/data/siqiouyang/download/llama3.1-8b-hf/
+w2v2_path=/mnt/taurus/data/xixu/models/wav2_vec_vox_960h_pl.pt
+# w2v2_path=/data/user_data/siqiouya/runs/pretrained/hubert_large_ll60k_finetune_ls960.pt
+w2v2_type=w2v2
+ctc_finetuned=True
+# data_path=/scratch/xixu/dataset/must-c-v1.0/en-es
+data_path=/mnt/aries/data/siqiouyang/datasets/must-c-v1.0/
+# data_path=/compute/babel-6-17/xixu/datasets/must-c-v1.0/en-fr
+source_lang="English"
+target_lang="German"
+name="3.1-8B-s1-${source_lang,,}-${target_lang,,}-${w2v2_type}-rope-bi"
+save_path=/mnt/taurus/data/siqiouyang/runs/sllama/en-de/$name
+rm -rf ${save_path}
+mkdir -p ${save_path}
+
+export PYTHONPATH=/home/siqiouyang/work/projects/sllama
+export WANDB_PROJECT="mustc_1.0_de"
+export WANDB_ENTITY="streamllama"
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+SLURM_GPUS=8
+
+cd /home/siqiouyang/work/projects/sllama/train
+torchrun --nproc_per_node=$SLURM_GPUS --rdzv-endpoint=0.0.0.0:9105 \
+    stage1_new.py \
+    --w2v2_path ${w2v2_path} \
+    --w2v2_type ${w2v2_type} \
+    --ctc_finetuned ${ctc_finetuned} \
+    --length_shrink_cfg "[(1024,2,2)] * 2" \
+    --block_size 1000000 \
+    --max_cache_size 1000000 \
+    \
+    --llm_path ${llm_path} \
+    --llm_freeze True \
+    \
+    --data_path ${data_path} \
+    --data_split_train 'train_st_de_fp' \
+    --data_split_eval 'dev_st_de_fp' \
+    --source_lang "${source_lang}" \
+    --target_lang "${target_lang}" \
+    \
+    --output_dir ${save_path} \
+    --num_train_epochs  6 \
+    --per_device_train_batch_size 3 \
+    --per_device_eval_batch_size 3 \
+    --gradient_accumulation_steps 11 \
+    --evaluation_strategy "steps" \
+    --eval_steps 200 \
+    --save_strategy "steps" \
+    --save_steps 200 \
+    --save_total_limit 4 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.2 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 5 \
+    --gradient_checkpointing True \
+    --seed 998244353 \
+    --report_to wandb \
+    --run_name $name \
+    --bf16 True \
+    --deepspeed ../configs/deepspeed_config_bf16.json
