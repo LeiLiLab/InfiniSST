084cfb85fd1fd590de66907d2399c744f122e185
Siqi Ouyang <owaski123@gmail.com>
2025-02-26 17:14:30 -0500

    data prep

--- data_prep/utils.py
+++ data_prep/utils.py
@@ -1,4 +1,5 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import csv
 import json
 
@@ -8,7 +9,13 @@ import matplotlib.pyplot as plt
 import sacrebleu
 import soundfile as sf
 
+import copy
 import yaml
+import math
+import torch
+from tqdm.notebook import tqdm
+
+from IPython.display import display, Audio
 
 def read_logs(path):
     logs = []
@@ -19,10 +26,19 @@ def read_logs(path):
                 logs.append(json.loads(l))
     return logs
 
+def write_logs(logs, path):
+    with open(path, "w") as w:
+        for log in logs:
+            w.write(json.dumps(log) + "\n")
+
 def read_wav(wav_path):
-    wav_path, offset, duration = wav_path.split(':')
-    offset = int(offset)
-    duration = int(duration)
+    if ':' in wav_path:
+        wav_path, offset, duration = wav_path.split(':')
+        offset = int(offset)
+        duration = int(duration)
+    else:
+        offset = 0
+        duration = -1
     source, rate = sf.read(wav_path, start=offset, frames=duration)
     return source, rate
 
@@ -53,3 +69,6 @@ def write_tsv(samples, tsv_path):
         )
         writer.writeheader()
         writer.writerows(samples)
+
+def play(audio_path):
+    display(Audio(read_wav(audio_path)[0], rate=16000))
